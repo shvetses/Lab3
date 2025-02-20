a7d509b4950a74c1b7be1769be74d5008df3eaf7
Екатерина Швец <shvetses@pik.ru>
2025-02-20 18:13:58 +0300

    fix

--- Task3_1/Program.cs
+++ Task3_1/Program.cs
@@ -8,6 +8,8 @@
             Console.WriteLine("Hello, World!");
             Console.WriteLine("Hello, World!");
             Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!");
         }
     }
 }
